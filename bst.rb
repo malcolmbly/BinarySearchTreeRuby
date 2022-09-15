44058e7592127fbd8e3ad2b479bcaa74164def78
malcolmbly <malcolmhbailey@gmail.com>
2022-09-15 12:27:22 +0100

    Added delete method (could probably be refactored)

--- bst.rb
+++ bst.rb
@@ -1,15 +1,14 @@
 class BST
   class Node
     include Comparable
-    attr_accessor :left, :right
-    attr_reader :data
-
+    attr_accessor :left, :right, :data
+  
     def initialize(left, right, data)
       @left = left
       @right = right
       @data = data
     end
-
+  
     def <=>(other)
       @data <=> other.data
     end
@@ -36,13 +35,58 @@ class BST
     root_node
   end
 
+  def insert(value, start_node = @root)
+    node_value = start_node.data
+    return if node_value == value
+
+    if start_node.right.nil? && value > node_value
+      start_node.right = Node.new(nil, nil, value)
+    elsif start_node.left.nil? && value < node_value
+      start_node.left = Node.new(nil, nil, value)
+    else
+      value > node_value ? insert(value, start_node.right) : insert(value, start_node.left)
+    end
+  end
+
+  def delete(value)
+    parent_node = nil
+    current_node = @root
+    node_to_delete = Node.new(nil, nil, value)
+    until current_node.nil?
+      if current_node == node_to_delete
+        @root = nil if current_node.nil?
+        if current_node.left.nil? && current_node.right.nil?
+          # node has no children
+          current_node > parent_node ? parent_node.right = nil : parent_node.left = nil
+        elsif current_node.left.nil? || current_node.right.nil?
+          # node has one child
+          current_node.left.nil? ? current_node = current_node.right : current_node = current_node.left
+          current_node > parent_node ? parent_node.right = current_node : parent_node.left = current_node
+        else
+          # node has two children
+          value < @root.data ? current_node = Node.new(current_node.left, nil, current_node.right.data) : current_node = Node.new(nil, current_node.right, current_node.left.data)
+          current_node > @root ? parent_node.right = current_node : parent_node.left = current_node
+        end
+      end
+      parent_node = current_node
+      node_to_delete > current_node ? current_node = current_node.right : current_node = current_node.left
+    end
+  end
+
+
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
-  
 end
 
-tree = BST.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree = BST.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 300, 450, 225, 221, 17, 18, 15])
+# tree.pretty_print
+tree.insert(100)
+tree.insert(7)
+tree.insert(2)
+tree.pretty_print
+tree.delete(450)
 tree.pretty_print
